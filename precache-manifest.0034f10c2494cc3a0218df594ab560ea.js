d5e57e1a7998cb6a9c9d95958bc76c3239f2acfe
LDQ <2693129576@qq.com>
2020-02-22 22:07:45 +0800

    ok

---
+++ precache-manifest.0034f10c2494cc3a0218df594ab560ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c39adb8a296ff753cbe",
+    "url": "/bookmoney-website/css/app.cf359a5f.css"
+  },
+  {
+    "revision": "91eebfb16df060b1e651e80194aae982",
+    "url": "/bookmoney-website/index.html"
+  },
+  {
+    "revision": "7c39adb8a296ff753cbe",
+    "url": "/bookmoney-website/js/app.86b54a93.js"
+  },
+  {
+    "revision": "7b97d826951f3d6e3eee",
+    "url": "/bookmoney-website/js/chunk-vendors.fd32d5c3.js"
+  },
+  {
+    "revision": "749f53835449f7e3f9e644454610ac14",
+    "url": "/bookmoney-website/manifest.json"
+  },
+  {
+    "revision": "bcab150d76b3fd3983f63d9b69eb1d3e",
+    "url": "/bookmoney-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/bookmoney-website/robots.txt"
+  }
+]);
